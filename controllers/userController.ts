2d3a8025be69569344c5fa50f3ed4a8e9ab32d6d
Maliyah Sangha <maliyah.sangha@gmail.com>
2025-10-14 16:27:59 -0700

    fixed id type in getUserById function

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -9,7 +9,7 @@ const getUserByEmailIdAndPassword = (email: string, password: string) => {
   }
   return null;
 };
-const getUserById = (id:any) => {
+const getUserById = (id: number) => {
   let user = userModel.findById(id);
   if (user) {
     return user;
